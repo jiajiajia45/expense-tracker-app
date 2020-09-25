51c2c6b8f7ca5c929a3fe8d2a0388fe511b07893
jaweria <jaweria.arshad.13579@gmail.com'                        git config --global user.name jawerua>
2020-09-25 07:46:42 +0500

    first commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,43 @@
+import React, { createContext, useReducer } from 'react';
+
+import AppReducer from './AppReducer';
+
+
+const initialState = {
+    transactions: []
+}
+
+
+export const GlobalContext = createContext(initialState);
+
+
+export const GlobalProvider = ({ children }) => {
+    const 
+        function delTransaction(id) {
+            dispatch({
+                type: 'DELETE_TRANSACTION',
+                payload: id
+            });
+        }
+
+       
+        function addTransaction(transaction) {
+            dispatch({
+                type: 'ADD_TRANSACTION',
+                payload: transaction
+            })
+        }
+
+    return (
+        <GlobalContext.Provider value={
+            {
+                transactions: state.transactions,
+                delTransaction,
+                addTransaction
+            }
+        }>
+            {children}
+        </GlobalContext.Provider>
+    );
+
+}
